6d6ca8ba48d57e699b6533ae2fe17f722c921642
Taimur Shah <tshah@scu.edu>
2018-11-05 03:08:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let final = [];
+  for (let i = 0; i < musicians.length; i++) {
+    final.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return final
+}
+
+function johnLennonFacts(array) {
+  let i = 0;
+  let newArray = [];
+  while (i < array.length) {
+    newArray.push(`${array[i]}!!!`);
+    i++;
+  } return newArray
+}
+
+function iLoveTheBeatles(number) {
+  let moreLove = [];
+  let i = 0
+  if (number < 15) {
+    do {
+      moreLove.push("I love the Beatles!");
+      i++;
+    } 
+    while ((number + i) < 15);
+    return moreLove
+} else {
+  moreLove.push("I love the Beatles!");
+  return moreLove
+  }
+}
